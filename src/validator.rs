ac488978f4037b095bbba5ea2b684c7e565e9153
Kyle Luzny <kluzny@gmail.com>
2023-03-18 09:07:14 -0500

    added clippy and it prefers ranges

--- src/validator.rs
+++ src/validator.rs
@@ -2,7 +2,7 @@ use crate::commands::GenerateArgs;
 
 pub fn validate_generator_args(gen_args: &GenerateArgs) -> Result<&GenerateArgs, String> {
     let length = gen_args.length;
-    if length < 1 || length > 256 {
+    if !(1..=256).contains(&length) {
         Err(String::from("length should be between 1 and 256"))
     } else {
         Ok(gen_args)
